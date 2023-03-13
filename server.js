5cb7a44cb3fac829e7bfc36d903335d420313ae7
Maria N. Reissig <nreissig@gmail.com>
2023-03-13 10:34:32 -0700

    Render the html through fs module

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-var http = require('http'); // 1 - Import Node.js core module
+// 1 - Import Node.js core module and File System module
+const http = require("http");
+const fs = require("fs");
 
-var server = http.createServer(function (req, res) {   // 2 - creating server
+// 2 - creating server
+const server = http.createServer((req, res) => {
+  //render the html file
+    fs.readFile("different-form-methods.html", (err, html) => {
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.write("404 Not Found");
+    } else {
+      res.write(html);
+    }
+    res.end();
+  });
 
-    //handle incomming requests here..
-
+//3 - listen for any incoming requests
 });
 
-server.listen(8080); //3 - listen for any incoming requests
-
-console.log('Node.js web server at port 8080 is running..')
+server.listen(8080);
+console.log("Node.js web server at port 8080 is running..");
