b64214ae19db97b5c3b660bf1ec2e6909ed1c81e
Maria N. Reissig <nreissig@gmail.com>
2023-03-16 11:16:42 -0700

    Set differents Response regarding Requests

--- server.js
+++ server.js
@@ -4,18 +4,26 @@ const fs = require("fs");
 
 // 2 - creating server
 const server = http.createServer((req, res) => {
+  const url = req.url;
   //render the html file
-    fs.readFile("different-form-methods.html", (err, html) => {
+
+  fs.readFile("different-form-methods.html", (err, html) => {
     if (err) {
       res.writeHead(404, { "Content-Type": "text/html" });
       res.write("404 Not Found");
-    } else {
-      res.write(html);
+    }
+    //3 - listen for any incoming requests
+    if (url === "/") {
+      res.writeHead(200), res.write(html);
+    }
+    if (url === "/resource-name/new") {
+      //do the stuff for GET the form
+    }
+    if (url === "/resource-name/") {
+      //do the stuff for POST the form
     }
     res.end();
   });
-
-//3 - listen for any incoming requests
 });
 
 server.listen(8080);
